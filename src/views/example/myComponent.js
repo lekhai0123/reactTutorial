aaa87885f7ccc11d152c7904127cc8594923b1de
Khải Lê <thanhkhai1234567890@gmail.com>
2024-07-17 12:38:55 +0700

    dom event

--- src/views/example/myComponent.js
+++ src/views/example/myComponent.js
@@ -1,21 +1,35 @@
 import React, { Fragment } from "react";
 class MyComponent extends React.Component {
-    state = {
-        name: 'Khải',
-        age: '20'
-    }
-    handleOnChangeName = (event) =>{
-        this.setState({
-            name: event.target.value
-        })
-    }
+  state = {
+    name: "Khải",
+    age: "20",
+  };
+  handleOnChangeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+  handleOnClick = () => {
+    alert("hé lô");
+  };
   render() {
     return (
       <>
-      <br></br>
-        <input value={this.state.name} type="text" onChange={(event)=> this.handleOnChangeName(event)}></input>
-        <h2>my name is {this.state.name}</h2>
-        <h2>i'm {this.state.age}</h2>
+        <div>
+          <br></br>
+          <input className="1"
+            value={this.state.name}
+            type="text"
+            onChange={(event) => this.handleOnChangeName(event)}
+          ></input>
+        </div>
+        <div>
+          <h2>my name is {this.state.name}</h2>
+          <h2>i'm {this.state.age}</h2>
+        </div>
+        <div>
+          <input className="2" value="click me" type="button" onClick={() => this.handleOnClick()}/>
+        </div>
       </>
     );
   }
